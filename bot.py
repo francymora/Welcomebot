c0ff7883c7630a7804fbddc89bb13553baa53ea8
francymora <79939741+francymora@users.noreply.github.com>
2021-09-13 15:20:51 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ def help(updater,context):
 
 def add_group(update: Update, context: CallbackContext):
     for member in update.message.new_chat_members:
-        update.message.reply_text(f'Hello {member.full_name}, benvenuto!' 
+        update.message.reply_text(f'Hello {member.full_name}, benvenuto!')
 
 
 add_group_handle = MessageHandler(Filters.status_update.new_chat_members, add_group)
